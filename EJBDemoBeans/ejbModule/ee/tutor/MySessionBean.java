44284e5579dec7ed010d09184c7e9e44a557b1c3
pavel <pavel@pavel>
2019-05-08 10:34:06 +0200

    schedule anno

--- EJBDemoBeans/ejbModule/ee/tutor/MySessionBean.java
+++ EJBDemoBeans/ejbModule/ee/tutor/MySessionBean.java
@@ -2,16 +2,17 @@ package ee.tutor;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Remote;
+import javax.ejb.Schedule;
 import javax.ejb.Stateful;
 import javax.ejb.Stateless;
 
 /**
  * Session Bean implementation class SessionBean
  */
-//@LocalBean
-//@Stateless
-@Stateful
-@Remote
+@LocalBean
+@Stateless
+//@Stateful
+//@Remote
 public class MySessionBean {
 
 	int count;
@@ -25,8 +26,15 @@ public class MySessionBean {
     }
 
     public String getData() {
-    	return "The very first response from Session Bean - iteration: " + (count++);
+    	//incCount();
+    	return "The very first response from Session Bean - iteration: " + getCount();
     }
+    
+    @Schedule(second = "*/5", minute = "*", hour = "*", persistent = false)
+    public void incCount() {
+    	count++;
+    }
+    
     
     public int getCount() {
     	return count;
