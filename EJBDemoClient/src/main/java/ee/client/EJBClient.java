ea016c7cb5f4599cb462db465dda3173a97da0cd
Pavel Petr <pavel.petr@hotmail.com>
2019-05-10 12:59:34 +0200

    funguje s maven

--- EJBDemoClient/src/main/java/ee/client/EJBClient.java
+++ EJBDemoClient/src/main/java/ee/client/EJBClient.java
@@ -4,18 +4,17 @@ import java.util.Properties;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
 public class EJBClient {
 
 	public static void main(String[] args) throws Exception {
 		Properties jndiProps = new Properties();
 		jndiProps.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
-		jndiProps.put(Context.PROVIDER_URL, "remote://localhost:8080");
-		// username
-		jndiProps.put(Context.SECURITY_PRINCIPAL, "peter");
-		// password
-		jndiProps.put(Context.SECURITY_CREDENTIALS, "lois");
+		jndiProps.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
+		// credentials
+		//jndiProps.put(Context.SECURITY_PRINCIPAL, "peter");
+		//jndiProps.put(Context.SECURITY_CREDENTIALS, "lois");
+		
 		// This is an important property to set if you want to do EJB invocations via
 		// the remote-naming project
 		jndiProps.put("jboss.naming.client.ejb.context", true);
@@ -23,8 +22,9 @@ public class EJBClient {
 		Context ctx = new InitialContext(jndiProps);
 		// lookup the bean Foo
 
-		Object bean = ctx.lookup("java:app/EJBDemoBeans/MySessionBean");
+		Object bean = null; 
+		//bean = ctx.lookup("java:app/EJBDemoBeans/MySessionBean");
+		bean = ctx.lookup("EJBDemoBeans/MySessionBean");
 		System.out.println("lookuped bean: " + bean);
 	}
-
 }
